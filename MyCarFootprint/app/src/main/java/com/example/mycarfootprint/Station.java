65ea76b8746a628fa0feb45c494cd8213098b6e3
unknown <zijinglu9@gmail.com>
2023-02-01 18:41:19 -0700

    first update

---
+++ MyCarFootprint/app/src/main/java/com/example/mycarfootprint/Station.java
@@ -0,0 +1,59 @@
+package com.example.mycarfootprint;
+
+import java.util.Date;
+
+public class Station {
+    private String name;
+    private Date date;
+    private String type;
+    private int amount;
+    private int price;
+
+    public Station (String name, Date date, String type, int amount, int price) {
+        this.name = name;
+        this.date = date;
+        this.type = type;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ MyCarFootprint/app/src/main/java/com/example/mycarfootprint/StationArrayAdapter.java
@@ -0,0 +1,4 @@
+package com.example.mycarfootprint;
+
+public class StationArrayAdapter {
+}
